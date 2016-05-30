6296207e63551e85e0af60ab15bb2f933c01958d
JamiePrentice <a.jamie.prentice@gmail.com>
2016-05-30 07:51:49 +0100

    Better-ish API routs

---
+++ app/models/gameQuery.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/* NPM Packages*/
+
+/* Imports */
+var Game = require('../models/game');
+var Query = require('../helpers/query');
+
+/* Variables */
+
+/* Functions */
+
+exports.getAll = function(request, response, next) {
+    Query.find(Game, {}, {time: 'descending'}, request, response, next);
+  };
+
+exports.get = function(gameId, request, response, next) {
+    Query.find(Game, {_id: request.params.id}, {}, request, response, next);
+  };
+
+exports.getByPlayer = function(playerName, request, response, next) {
+    Query.find(Game,
+                {$or:
+                    [{player1: playerName},
+                    {player2: playerName}]
+    },
+                {time: 'descending'},
+                request,
+                response,
+                next);
+  };
+
+exports.getByPlayers = function(player1Name, player2Name, request, response, next) {
+    Query.find(Game,
+                {$or:
+                    [{player1: player1Name, player2: player2Name},
+                    {player1: player2Name, player2: player1Name}]
+    },
+                {time: 'descending'},
+                request,
+                response,
+                next);
+  };
--- app/server/routes/api.game.routes.js
+++ app/server/routes/api.game.routes.js
@@ -3,8 +3,7 @@
 /* NPM Packages*/
 
 /* Imports */
-var Game = require('../../models/game');
-var Query = require('../../helpers/query');
+var Game = require('../../models/gameQuery');
 
 /* Variables */
 
@@ -13,34 +12,22 @@ module.exports = function(Router) {
 
     /* All games */
     Router.get('/api/games', function(request, response) {
-        Query.find(Game, {}, {time: 'descending'}, response);
+        Game.getAll(request, response);
       });
 
     /* Game by ID */
-    Router.get('/api/games/id/:id', function(request, response) {
-        Query.find(Game, {_id: request.params.id}, {}, response);
+    Router.get('/api/games/:id', function(request, response) {
+        Game.get(request.params.id, request, response);
       });
 
     /* Games by player name */
     Router.get('/api/games/player/:name', function(request, response) {
-        Query.find(Game,
-                    {$or:
-                        [{player1: request.params.name},
-                        {player2: request.params.name}]
-                    },
-                    {},
-                    response);
+        Game.getByPlayer(request.params.name, request, response);
       });
 
     /* Find player by name - with the option for opponents as a param ie. ?opponent= */
-    Router.get('/api/games/player/:name', function(request, response) {
-        Query.find(Game,
-                    {$or:
-                        [{player1: request.params.name, player2: request.query.opponent},
-                        {player1: request.query.opponent, player2: request.params.name}]
-                    },
-                    {},
-                    response);
+    Router.get('/api/games/players/:name', function(request, response) {
+        Game.getByPlayers(request.params.name, request.query.opponent, request, response);
       });
 
   };
