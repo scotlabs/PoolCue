64a972c6084020bd103f09c793958d55974db51c
JamiePrentice <a.jamie.prentice@gmail.com>
2016-06-10 08:04:53 +0100

    Added compression for faster loading.
    - 572kb -> 335kb on initial load.

--- app/server/server.js
+++ app/server/server.js
@@ -1,11 +1,12 @@
 'use strict';
 
 /* NPM Packages*/
-var BodyParser = require('body-parser');
-var Express    = require('express');
-var Helmet     = require('helmet');
-var Https      = require('https');
-var Http       = require('http');
+var Compression = require('compression');
+var BodyParser  = require('body-parser');
+var Express     = require('express');
+var Helmet      = require('helmet');
+var Https       = require('https');
+var Http        = require('http');
 
 /* Imports */
 var Logger = require('../helpers/logger');
@@ -19,6 +20,9 @@ var App = new Express();
 exports.start = function(homeDirectory) {
     // Database
     require('../server/database').connect();
+    // Routes
+    require('./routes/routes')(Router, Logger);
+    // Port config
     var httpPort  = process.env.port || 8080;
     var httpsPort = process.env.port || 8081;
     var options   = {
@@ -27,15 +31,18 @@ exports.start = function(homeDirectory) {
       // ca:   _fs.readFileSync('./app/config/certs/chain.pem')
     };
 
-    require('./routes/routes')(Router, Logger);
     try {
+      // Security
       App.use(new Helmet());
       App.use(Helmet.hidePoweredBy());
-
+      // Compression
+      App.use(Compression());
       //App.use(Server.requireHTTPS);
+      // Static Files
       App.use('/', Express.static(homeDirectory + '/bower_components'));
       App.use('/', Express.static(homeDirectory + '/app/public'));
       Express.static(homeDirectory + '/public');
+
       App.set('view engine', 'ejs');
       App.use(BodyParser.urlencoded({extended: true}));
       App.use('/', Router);
