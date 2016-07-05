ec67f8e6368397781e8bd9c02a1a28c07bed105c
JamiePrentice <a.jamie.prentice@gmail.com>
2016-07-05 17:11:06 +0100

    Missed equals symbol.

--- app/models/methods/player.js
+++ app/models/methods/player.js
@@ -44,7 +44,7 @@ exports.getStats = function(playerName, socket, request, response) {
 
 function getLast10Games(playerName, games) {
   var last10games = [];
-  for (var i = games.length; i > 0; i--) {
+  for (var i = games.length; i >= 0; i--) {
     if (games[i] && last10games.length < 10) {
       if (games[i].winner === playerName) {
         last10games.push(true);
