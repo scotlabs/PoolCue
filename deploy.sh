967974862cc935a274b1a11fc56839d1bc916a17
JamiePrentice <a.jamie.prentice@gmail.com>
2016-07-20 17:58:04 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -117,12 +117,7 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd - > /dev/null
 fi
 
-if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
-    cd "$DEPLOYMENT_TARGET"
-    eval bower install
-    exitWithMessageOnError "bower failed"
-    cd - > /dev/null
-fi
+
 ##################################################################################################################################
 
 # Post deployment stub
