8a13dce1c5acd3aec805412f3a108dc2fb57a35b
JamiePrentice <a.jamie.prentice@gmail.com>
2016-07-20 17:39:25 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -117,7 +117,7 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd - > /dev/null
 fi
 
-if [ -e "%DEPLOYMENT_TARGET%\bower.json" ]; then
+if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
     cd "$DEPLOYMENT_TARGET"
     eval $NPM_CMD bower install
     exitWithMessageOnError "npm failed"
