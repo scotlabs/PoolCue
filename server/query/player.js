4a4d498912e816544767387ac93b7f87c7004e06
Jamie Prentice <a.jamie.prentice@gmail.com>
2016-09-01 13:33:12 +0100

    Filtered Players Number from the API calls

--- server/query/player.js
+++ server/query/player.js
@@ -3,20 +3,21 @@
 /* NPM Packages*/
 
 /* Imports */
-var Player        = require('../models/player');
-var PlayerMethods = require('../models/methods/player');
+const Player        = require('../models/player');
+const PlayerMethods = require('../models/methods/player');
 
 /* Variables */
+const filter = {__v: 0, mobile_number: 0, enableNotification: 0};
 
 /* Functions */
 exports.getAll = function(request, response) {
-    Player.find({}, {__v: 0}).sort({elo: 'descending'}).lean().exec(function(error, result) {
+    Player.find({}, filter).sort({elo: 'descending'}).lean().exec(function(error, result) {
           response.json(result);
         });
   };
 
 exports.get = function(playerName, request, response) {
-    Player.find({name: playerName}, {__v: 0}).lean().exec(function(error, result) {
+    Player.find({name: playerName}, filter).lean().exec(function(error, result) {
           response.json(result);
         });
   };
