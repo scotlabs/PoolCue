18cf99369d2b878a8e79a37b6d240362c08e3629
Jamie <jamieprentice@Jamies-MacBook-Air.local>
2016-11-27 18:37:43 +0000

    API endpoints for creating and deleting games
    Should parameter validation be in the route?

--- server/routes/api/game.command.js
+++ server/routes/api/game.command.js
@@ -7,14 +7,29 @@ var Game = require('../../models/methods/game');
 module.exports = function (Router, io) {
 
 	Router.post('/games/create', function (request, response) {
-		Game.queue(request.query.player1, request.query.player2, io);
+		if(request.query.player1 && request.query.player2){
+			Game.queue(request.query.player1, request.query.player2, io);
+			response.sendStatus(200);
+		}else{
+			response.status(400).send('Missing parameter data');
+		}
 	});
 
 	Router.post('/games/delete', function (request, response) {
-		Game.queue(request.query.gameId, io);
+		if(request.query.gameId){
+			Game.queue(request.query.gameId, io);
+			response.sendStatus(200);
+		}else{
+			response.status(400).send('Missing parameter data');
+		}
 	});
 
 	Router.post('/games/complete', function (request, response) {
-		Game.complete(request.query.gameId, request.query.winner, io);
+		if(request.query.gameId && request.query.winner){
+			Game.complete(request.query.gameId, request.query.winner, io);
+			response.sendStatus(200);
+		}else{
+			response.status(400).send('Missing parameter data');
+		}
 	});
 };
