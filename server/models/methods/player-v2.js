d768c9fdc47c1955c4e48fefb6b6d0a2b4fc9e93
Jamie Prentice <jamieprentice@Jamies-MacBook-Air.local>
2016-11-27 23:03:38 +0000

    Scaffolding for simplification

---
+++ server/models/methods/player-v2.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* NPM Packages*/
+
+/* Imports */
+
+/* Global Variables */
+
+/* Functions */
+exports.create = function(name){
+
+}
+
+exports.delete = function(){
+
+}
+
+exports.update = function(elo, wins, losses){
+
+}
