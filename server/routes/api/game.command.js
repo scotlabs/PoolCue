a1f6c5f30e3aea6dd602c4a91dcc932a90c5fea7
Jamie Prentice <jamieprentice@Jamies-MacBook-Air.local>
2016-11-27 21:57:47 +0000

    Added root URLs to API Routes

--- server/routes/api/game.command.js
+++ server/routes/api/game.command.js
@@ -5,8 +5,9 @@ var Game = require('../../models/methods/game');
 
 /* Routes */
 module.exports = function (Router, io) {
+	var root = '/api/games/'
 
-	Router.post('/games/create', function (request, response) {
+	Router.post(root + 'create', function (request, response) {
 		if(request.query.player1 && request.query.player2){
 			Game.queue(request.query.player1, request.query.player2, io);
 			response.sendStatus(200);
@@ -15,7 +16,7 @@ module.exports = function (Router, io) {
 		}
 	});
 
-	Router.post('/games/delete', function (request, response) {
+	Router.post(root + 'delete', function (request, response) {
 		if(request.query.gameId){
 			Game.queue(request.query.gameId, io);
 			response.sendStatus(200);
@@ -24,7 +25,7 @@ module.exports = function (Router, io) {
 		}
 	});
 
-	Router.post('/games/complete', function (request, response) {
+	Router.post(root + 'complete', function (request, response) {
 		if(request.query.gameId && request.query.winner){
 			Game.complete(request.query.gameId, request.query.winner, io);
 			response.sendStatus(200);
