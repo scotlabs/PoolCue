d72edf394c19602517bebf5d163f87810a7fd669
Jamie Prentice <a.jamie.prentice@gmail.com>
2016-11-29 09:13:11 +0000

    Uncommmited API examples.

--- server/routes/api/game.query.js
+++ server/routes/api/game.query.js
@@ -1,43 +1,46 @@
 'use strict';
 
-/* NPM Packages*/
-
 /* Imports */
 var Game = require('../../query/game');
 var GameHelper = require('../../helpers/game');
-
-/* Variables */
 
 /* Routes */
 module.exports = function (Router) {
 	var root = '/api/games/'
 
-	/* All games */
+	// Endpoint: All Games
+	// Example: GET - example.com/api/games
 	Router.get(root, function (request, response) {
 		Game.getAll(request, response);
 	});
 
-	/* Games in the queue */
+	// Endpoint: All Games currently in the Queue
+	// Example: GET - example.com/api/games/queue
 	Router.get(root + 'queue', function (request, response) {
 		Game.getQueue(request, response);
 	});
 
-	/* Games currently playing */
+	// Endpoint: The Game currently being played
+	// Example: GET - example.com/api/games/current
 	Router.get(root + 'current', function (request, response) {
 		Game.getPlaying(request, response);
 	});
 
-	/* Games that are complete */
+	// Endpoint: All Games that are complete
+	// Example: GET - example.com/api/games/complete
 	Router.get(root + 'complete', function (request, response) {
 		Game.getComplete(request, response);
 	});
 
-	/* Game by ID */
+	// Endpoint: A game by Id.
+	// Example: GET - example.com/api/games/id/183c519f7b99fceab00820570
 	Router.get(root + 'id/:id', function (request, response) {
 		Game.get(request.params.id, request, response);
 	});
 
-	/* Find games by player name - with the option for opponents as a param ie. ?opponent= */
+	// Endpoint: A player by name or find player with an specific opponent
+	// Example: GET - example.com/api/games/player/Fred
+	// Example: GET - example.com/api/games/player/Fred?opponent=George
 	Router.get(root + 'player/:name', function (request, response) {
 		var playerName = GameHelper.formatName(request.params.name);
 		if (request.query.opponent) {
